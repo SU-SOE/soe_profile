922b1053f39077da930504ebabe66215c582aa53
Ian Monroe <ian@ianmonroe.com>
2020-05-20 23:03:14 +0000

    Correcting typo in test

--- tests/src/Unit/Config/ConfigOverridesTest.php
+++ tests/src/Unit/Config/ConfigOverridesTest.php
@@ -31,7 +31,7 @@ class ConfigOverridesTest extends UnitTestCase {
   }
 
   public function testConfigOverrides() {
-    $this->assertEquals('StanfordProfileConfigOverride', $this->overrideService->getCacheSuffix());
+    $this->assertEquals('SoeProfileConfigOverride', $this->overrideService->getCacheSuffix());
     $this->assertNull($this->overrideService->createConfigObject('foo'));
     $this->assertInstanceOf(CacheableMetadata::class, $this->overrideService->getCacheableMetadata('foo'));
 
