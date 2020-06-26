7df605c66f1157a234c18b011981d6f4a57f6563
Ian Monroe <ian@ianmonroe.com>
2020-06-26 16:03:33 -0600

    tweak

--- tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
+++ tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
@@ -57,7 +57,7 @@ class StanfordImageCTACest {
   public function testCtaList(\AcceptanceTester $I) {
     $node = $this->createNodeWithParagraph($I);
     $I->amOnPage($node->toUrl()->toString());
-    $I->seeElement(Locator::contains("//div[@class='su-image-cta-paragraph__image']//img"));
+    $I->seeElement("//div[@class='su-image-cta-paragraph__image']//img");
     $I->seeLink('Link Alpha', 'http://google.com');
   }
 
