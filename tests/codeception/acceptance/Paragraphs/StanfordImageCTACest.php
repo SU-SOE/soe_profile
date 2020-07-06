569522ff29f4bed66e433aefd70bdaa30202fb92
Ian Monroe <ian@ianmonroe.com>
2020-07-06 09:35:33 -0600

    D8CORE-2273: working on internal link test

--- tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
+++ tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
@@ -5,6 +5,9 @@
  */
 class StanfordImageCTACest {
 
+
+  protected $paragraphs;
+
   /**
    * Create a CTA List paragraph to test.
    */
@@ -27,38 +30,46 @@ class StanfordImageCTACest {
         'target_id' => '4',
       ],
       'stanford_image_cta_link' => [
-        'uri' => '<front>',
+        'uri' => '',
         'title' => 'Internal Link test',
       ],
     ], 'paragraph');
 
-    return $paragraphs;
+    $this->paragraphs = $paragraphs;
   }
 
   /**
    * Create a node to hold the paragraph.
    */
   protected function createNodeWithParagraph(\AcceptanceTester $I) {
+    $nodes = [];
     $paragraph = $this->createParagraphs($I);
-    $node = $I->createEntity([
+    $nodes[0] = $I->createEntity([
       'type' => 'stanford_page',
       'title' => 'Test Image CTA',
       'su_page_components' => [
-        'target_id' => $paragraphs[0]->id(),
-        'entity' => $paragraph[0],
+        'target_id' => $this->paragraphs[0]->id(),
+        'entity' => $this->paragraphs[0],
         'settings' => json_encode([
           'row' => 0,
           'index' => 0,
           'width' => 12,
           'admin_title' => 'Image CTA',
         ]),
-        'target_id' => $paragraphs[1]->id(),
-        'entity' => $paragraph[1],
+      ],
+    ]);
+
+    $nodes[0] = $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => 'Test Image CTA',
+      'su_page_components' => [
+        'target_id' => $this->paragraphs[1]->id(),
+        'entity' => $this->paragraphs[1],
         'settings' => json_encode([
           'row' => 0,
           'index' => 0,
           'width' => 12,
-          'admin_title' => 'Image CTA - internal link',
+          'admin_title' => 'Image CTA',
         ]),
       ],
     ]);
@@ -71,10 +82,22 @@ class StanfordImageCTACest {
    * Test the Image CTA paragraph in the page.
    */
   public function testImageCta(\AcceptanceTester $I) {
-    $node = $this->createNodeWithParagraph($I);
-    $I->amOnPage($node->toUrl()->toString());
+    $nodes = $this->createNodeWithParagraph($I);
+    $I->amOnPage($nodes[0]->toUrl()->toString());
     $I->seeElement("//div[contains(@class, 'su-image-cta-paragraph__image')]");
     $I->seeLink('Link Alpha', 'http://google.com');
+    $I->see('Internal Link test');
+
+    $I->logInWithRole('administrator');
+    $I->amOnPage($nodes[1]->toUrl()->toString());
+    $I->click('.edit .tabs__tab a');
+    $I->see('Image CTA - internal link');
+    $I->click("//div[@id='react-su-page-components']//button[@class='button']");
+    $I->see('The image to display');
+    $I->fillField("//input[contains(@id, 'stanford_image_cta_link-uri-0')]", '<front>');
+    $I->click('Continue');
+    $I->click('edit-submit');
+    $I->amOnPage($nodes[1]->toUrl()->toString());
     $I->see('Internal Link test');
     $I->click('Internal Link test');
     $I->amOnPage('/');
