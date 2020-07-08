76aff7cdb4034f4918e6bc07b60f77617fbaf237
Ian Monroe <ian@ianmonroe.com>
2020-07-08 13:38:14 -0600

    Fixup!

--- tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
+++ tests/codeception/acceptance/Paragraphs/StanfordImageCTACest.php
@@ -94,7 +94,7 @@ class StanfordImageCTACest {
 
     $I->logInWithRole('site_manager');
     $I->amOnPage($nodes[1]->toUrl()->toString());
-    $I->click('//li[contains(@class, "edit tabs_tab")]/a');
+    $I->click('/html/body/div[2]/div[2]/div[2]/nav/ul/li[3]/a');
     $I->see('Image CTA - internal link');
     $I->click("//div[@id='react-su-page-components']//button[@class='button']");
     $I->see('The image to display');
