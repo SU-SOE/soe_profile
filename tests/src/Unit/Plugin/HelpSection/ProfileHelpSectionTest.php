9095e50b85d6ad49a7d346ca62c68dd0d32f1c39
Mike Decker <pookmish@gmail.com>
2023-09-21 14:57:43 -0700

    Fixed tests for D10

--- tests/src/Unit/Plugin/HelpSection/ProfileHelpSectionTest.php
+++ tests/src/Unit/Plugin/HelpSection/ProfileHelpSectionTest.php
@@ -19,7 +19,7 @@ class ProfileHelpSectionTest extends UnitTestCase {
   /**
    * {@inheritDoc}
    */
-  protected function setUp() {
+  public function setUp(): void {
     parent::setUp();
     $container = new ContainerBuilder();
     $container->set('string_translation', $this->getStringTranslationStub());
